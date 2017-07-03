0675c4af611789f27736c82e2cbec16e47586304
Aaron Glenn <aglenn@laureateinstitute.org>
2017-07-03 14:47:43 -0500

    Get-WyseTerminalLeases.ps1

---
+++ Get-WyseTerminalLeases.ps1
@@ -0,0 +1,14 @@
+﻿# PURPOSE: Find all leases likely to belong to a wyse thin client
+
+# Commonly Modified options
+$DHCPServers="Server1.contoso.com","Server2.contoso.com"
+
+$ThisDir=$PSScriptRoot
+. $PSScriptRoot\DHCPHelperFunctions.ps1
+
+if (!$Scopes) {$Scopes=Get-IPv4Scopes -Servers $DHCPServers -Unique $false}
+if (!$Leases) {$Leases=Get-IPv4Leases -Scopes $Scopes}
+$ThinClientLeases=$Leases|Where-Object {$_.ClientId -like "00-80-64-*"}
+
+$Leases|Export-Csv -Path "$PSScriptRoot\AllLeases.csv" -NoTypeInformation
+$ThinClientLeases|Export-Csv -Path "$PSScriptRoot\WyseLeases.csv" -NoTypeInformation
